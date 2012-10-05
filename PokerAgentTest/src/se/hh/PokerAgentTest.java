9b5f5f17dda37b1792999de356cb178fb7bfb022
OloloshkaOloloev <kubrakov90@gmail.com>
2012-10-05 13:58:01 +0200

    Added test project;

---
+++ PokerAgentTest/src/se/hh/PokerAgentTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.hh;
+
+import com.medicwave.cardgame.poker.Agent;
+
+/**
+ *
+ * @author Admin
+ */
+public class PokerAgentTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Agent agent = new Agent();
+    }
+}
